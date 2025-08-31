c9564dc70e586417f8530f586fe5ce5d6fd7ff4a
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-09-01 06:31:41 +0700

    Hello World belajar CI4

---
+++ HelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class HelloWorld extends Controller
+{
+    public function index()
+    {
+        return '<h1>Hello World!</h1>';
+    }
+}
+
